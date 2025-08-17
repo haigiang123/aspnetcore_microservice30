84ad11c47b9e05d54ca332a12b46434af7d69df1
giangthanhhai17 <giangthanhhai17@gmail.com>
2025-08-17 23:11:41 +0700

    2.1: Khởi tạo các Entity, Dtos, Repository Pattern cho Product Microservices 2.2 Triển khai các phương thức CRUD cho Product.API 2.3  Chạy thử Product.API Microservices trong môi trường Development 2.4 2.4: Containerize Product.API Microservices với MySQL

---
+++ src/Services/Product/Product.API/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using Contracts.Common.Interfaces;
+using Product.API.Entities;
+using Product.API.Persistence;
+
+namespace Product.API.Repositories.Interfaces
+{
+    public interface IProductRepository : IRepositoryBaseAsync<CatalogProduct, long, ProductContext>
+    {
+        public Task<IEnumerable<CatalogProduct>> GetProducts();
+        public Task<CatalogProduct> GetProduct(long id);
+
+        public Task<CatalogProduct> GetProductByNo(string productNo);
+        public Task CreateProduct(CatalogProduct product);
+        public Task UpdateProduct(CatalogProduct product);
+        public Task DeleteProduct(long id);
+    }
+}
