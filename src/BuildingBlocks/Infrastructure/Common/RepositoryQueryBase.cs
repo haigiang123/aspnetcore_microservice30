d6f5c6fee09579495c64f690a9eb7058a42a406f
giangthanhhai17 <giangthanhhai17@gmail.com>
2025-10-06 15:37:05 +0700

    3.1: Tìm hiểu về PostgreSQL 3.2: Khởi tạo Project Web API và các Entity, Dtos cho Customer.API Microservices 3.3: Xây dựng Repository Pattern cho Customer.API Microservices 3.4: Triển khai các CRUD API cho Customer.API Microservices 3.5: Chạy thử Customer.API Microservices trong môi trường Development 3.6: Containerize Customer.API Microservices với PostgreSQL

---
+++ src/BuildingBlocks/Infrastructure/Common/RepositoryQueryBase.cs
@@ -0,0 +1,58 @@
+﻿using Contracts.Common.Interfaces;
+using Contracts.Domains;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Common
+{
+    public class RepositoryQueryBase<T, K, TContext> : IRepositoryQueryBase<T, K, TContext>
+        where T : EntityBase<K>
+        where TContext : DbContext
+    {
+        private readonly TContext _context;
+
+        public RepositoryQueryBase(TContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public IQueryable<T> FindAll(bool trackChanges = false)
+        {
+            return !trackChanges ? _context.Set<T>().AsNoTracking() : _context.Set<T>();
+        }
+
+        public IQueryable<T> FindAll(bool trackChanges = false, params Expression<Func<T, object>>[] includeProperties)
+        {
+            var items = this.FindAll(trackChanges);
+            items = includeProperties.Aggregate(items, (current, includeProperty) => current.Include(includeProperty));
+            return items;
+        }
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> condition, bool trackChanges = false)
+        {
+            return !trackChanges ? _context.Set<T>().Where(condition).AsNoTracking() : _context.Set<T>().Where(condition);
+        }
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> condition, bool trackChanges = false, params Expression<Func<T, object>>[] includeProperties)
+        {
+            var items = this.FindByCondition(condition, trackChanges);
+            items = includeProperties.Aggregate(items, (current, includeProperty) => current.Include(includeProperty));
+            return items;
+        }
+
+        public async Task<T?> GetByIdAsync(K id)
+        {
+            return await FindByCondition(x => x.Id.Equals(id)).FirstOrDefaultAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(K id, params Expression<Func<T, object>>[] includeProperties)
+        {
+            return await FindByCondition(x => x.Id.Equals(id), false, includeProperties).FirstOrDefaultAsync();
+        }
+    }
+}
--- src/Services/Customer/Customer.API/Program.cs
+++ src/Services/Customer/Customer.API/Program.cs
@@ -1,17 +1,95 @@
+using Serilog;
+using Common.Logging;
+using Contracts.Common.Interfaces;
+using Customer.API.Persistence;
+using Customer.API.Repositories;
+using Customer.API.Repositories.Interface;
+using Customer.API.Service;
+using Customer.API.Service.Interface;
+using Infrastructure.Common;
+using Microsoft.EntityFrameworkCore;
+using Customer.API;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog(Serilogger.Configure);
 
-// Add services to the container.
+Log.Information("Start Customer API up");
 
-builder.Services.AddControllers();
+try
+{
+    // Add services to the container.
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+    var connectstring = builder.Configuration.GetConnectionString("DefaultConnectionString");
+    builder.Services.AddDbContext<CustomerContext>(
+        options => options.UseNpgsql(connectstring));
 
-// Configure the HTTP request pipeline.
+    builder.Services.AddScoped<ICustomerRepository, CustomerRepository>()
+                    .AddScoped(typeof(IRepositoryBase<,,>), typeof(RepositoryQueryBase<,,>))
+                    .AddScoped(typeof(IUnitOfWork<>), typeof(UnitOfWork<>))
+                    .AddScoped<ICustomerService, CustomerService>();
 
-app.UseHttpsRedirection();
+    var app = builder.Build();
 
-app.UseAuthorization();
+    //// Syntax minimal api 
+    app.MapCustomerApi();
+    ///we can use in Program.cs or controller
+    //app.MapGet("/api/customers",
+    //    async (ICustomerService customer) => await customer.GetCustomersAsync());
+    //app.MapGet("/api/customers/{username}",
+    //    async (string username, ICustomerService customer) => await customer.GetCustomerByUsernameAsync(username));
+    //app.MapPost("", async (Customer.API.Entities.Customer customer, ICustomerRepository repository) =>
+    //{
+    //    await repository.CreateAsync(customer);
+    //    await repository.SaveChangesAsync();
+    //});
+    //app.MapDelete("/api/customers/{userId}",
+    //    async (int userId, ICustomerRepository customer) =>
+    //    {
+    //        var user = await customer.FindByCondition(x => x.Id.Equals(userId)).SingleOrDefaultAsync();
 
-app.MapControllers();
+    //        if (user == null)
+    //        {
+    //            return Results.NotFound();
+    //        }
 
-app.Run();
+    //        await customer.DeleteAsync(user);
+    //        await customer.SaveChangesAsync();
+
+    //        return Results.NoContent();
+    //    });
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    //app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.SeedCustomerData().Run();
+    //app.Run();
+
+}
+catch (Exception ex)
+{
+    string type = ex.GetType().Name;
+    if (type.Equals("StopTheHostException", StringComparison.Ordinal))
+    {
+        throw;
+    }
+
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down Product API complete");
+    Log.CloseAndFlush();
+}
+
