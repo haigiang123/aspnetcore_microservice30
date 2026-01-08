892f4550630ae0ff69118c2fecc7500ca4cb9c35
giangthanhhai17 <giangthanhhai17@gmail.com>
2026-01-08 13:48:03 +0700

    8.1 API Gateway and Routing Pattern 8.2 - 8.3 Ocelot API Gateway 8.4 Ocelot.json 8.8 Rate Limiting 8.9 Quality of Service 8.10 Caching 8.11 Config swagger for Ocelot

---
+++ src/ApiGateways/OcelotApiGw/Extensions/HostExtension.cs
@@ -0,0 +1,17 @@
+﻿namespace OcelotApiGw.Extensions
+{
+    public static class HostExtension
+    {
+        public static void AddHostExtensions(this ConfigureHostBuilder host) 
+        {
+            host.ConfigureAppConfiguration((context, config) =>
+            {
+                var env = context.HostingEnvironment;
+                config.AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, true)
+                .AddJsonFile($"ocelot.{env.EnvironmentName}.json", false, true)
+                .AddEnvironmentVariables();
+            });
+        }
+    }
+}
--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,17 +1,62 @@
+using Common.Logging;
+using Infrastructure.Middlewares;
+using Ocelot.Middleware;
+using OcelotApiGw.Extensions;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateBootstrapLogger();
+Log.Information("Start Product API up");
 
-// Add services to the container.
+try
+{
+    builder.Host.UseSerilog(Serilogger.Configure);
+    builder.Host.AddHostExtensions();
 
-builder.Services.AddControllers();
+    // Add services to the container.
+    builder.Services.AddControllers();
+    builder.Services.AddServiceExtentions(builder.Configuration);
+    builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+    var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+    // Configure the HTTP request pipeline.
+    app.UseCors();
+    app.UseMiddleware<ErrorWrappingMiddleware>();
+    app.UseRouting();
 
-app.UseHttpsRedirection();
+    //app.UseHttpsRedirection();
+    //app.UseAuthorization();
 
-app.UseAuthorization();
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapGet("/", context =>
+        {
+            // await context.Response.WriteAsync($"Hello TEDU members! This is {builder.Environment.ApplicationName}");
+            context.Response.Redirect("swagger/index.html");
+            return Task.CompletedTask;
+        });
+    });
 
-app.MapControllers();
+    app.UseSwaggerForOcelotUI(
+        opt => { opt.PathToSwaggerGenerator = "/swagger/docs"; });
+    await app.UseOcelot();
 
-app.Run();
+    app.Run();
+
+}
+catch (Exception ex)
+{
+    string type = ex.GetType().Name;
+    if (type.Equals("StopTheHostException", StringComparison.Ordinal))
+    {
+        throw;
+    }
+
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down Product API complete");
+    Log.CloseAndFlush();
+}
